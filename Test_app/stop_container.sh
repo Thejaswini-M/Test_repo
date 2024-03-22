6e9682cfe57beaec651afba5486d04e60639ddb3
Thejaswini-M <116334582+Thejaswini-M@users.noreply.github.com>
2024-03-22 15:46:39 +0530

    Update stop_container.sh

--- Test_app/stop_container.sh
+++ Test_app/stop_container.sh
@@ -1,7 +1,7 @@
 set -e
 echo
-## to stop any container running on port
-#!/bin/bash
+# ## to stop any container running on port
+# #!/bin/bash
 
-containerid='docker ps|awk-F ""'{Print$1}''
-docker rm -f $containerid
+# containerid='docker ps|awk-F ""'{Print$1}''
+# docker rm -f $containerid
