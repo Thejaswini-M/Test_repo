8f20442d8726b21194138cfc5c8abeb0d1b6a450
Thejaswini-M <116334582+Thejaswini-M@users.noreply.github.com>
2024-03-27 21:55:09 +0530

    Update stop_container.sh

--- Test_app/stop_container.sh
+++ Test_app/stop_container.sh
@@ -4,8 +4,20 @@ set -e
 # Stop the running container (if any)
 echo "Started application start now"
 chmod +x "$0"
-echo "Hi"
-echo "Hello"
-containerid=$(docker ps -q)
-echo $containerid
-docker rm -f $containerid
+
+# Get a list of running container IDs
+CONTAINER_IDS=$(docker ps --format '{{.ID}}')
+
+# Check if there are any running containers
+if [ -n "$CONTAINER_IDS" ]; then
+    # Iterate over each container and stop it
+    for CONTAINER_ID in $CONTAINER_IDS; do
+        CONTAINER_NAME=$(docker inspect --format '{{.Name}}' "$CONTAINER_ID" | sed 's#^/##')
+        echo "Stopping container: $CONTAINER_NAME"
+        docker stop "$CONTAINER_ID"
+        echo "Container $CONTAINER_NAME stopped successfully."
+    done
+else
+    echo "No running containers found."
+fi
+
