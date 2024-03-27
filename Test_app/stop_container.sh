8a79c98a2ae7a718f3fdc11d5867f5b57d16ee35
Thejaswini-M <116334582+Thejaswini-M@users.noreply.github.com>
2024-03-27 16:29:51 +0530

    Update stop_container.sh

--- Test_app/stop_container.sh
+++ Test_app/stop_container.sh
@@ -1,10 +1,10 @@
 
+#!/bin/bash
 set -e
-
 # Stop the running container (if any)
+chmod +x
 echo "Hi"
 echo "Hello"
-#!/bin/bash
 containerid=$(docker ps -q)
 echo $containerid
 docker rm -f $containerid
