6c126972b697154ecbc537e5ef5151e7e3d3b318
Thejaswini-M <116334582+Thejaswini-M@users.noreply.github.com>
2024-03-27 13:42:30 +0530

    Update stop_container.sh

--- Test_app/stop_container.sh
+++ Test_app/stop_container.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+
 set -e
 
 # Stop the running container (if any)
 echo "Hi"
-
+echo "Hello"
+#!/bin/bash
 containerid=$(docker ps|awk -F " " 'N>1{print$1}')
-echo containerid
+echo $containerid
 docker rm -f $containerid
